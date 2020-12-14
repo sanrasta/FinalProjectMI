79ea2a900b3b17345d5f414a8e53e9fa81bf6525
Justin-WR-West <60618699+Justin-WR-West@users.noreply.github.com>
2020-12-13 19:03:35 -0500

    Up to problem 7 specified

--- src/interface/ImageAcquisitionDriver.py
+++ src/interface/ImageAcquisitionDriver.py
@@ -6,48 +6,163 @@ import src.project.Utilities as util
 
 cardiac = util.loadImage("images/cardiac.jpg")
 util.displayImage(cardiac)
-
 normal_cardiac = util.normalizeImage(cardiac)
-
 dft_cardiac = util.getDFT(normal_cardiac)
-
+util.saveMatrix("Cardiac_DFT", dft_cardiac)
 height, width = cardiac.shape
 cardiac_size = np.array([height, width])
 
+
 brain = util.loadImage("images/brain.png")
 util.displayImage(brain)
-
 normal_brain = util.normalizeImage(brain)
-
 dft_brain = util.getDFT(normal_brain)
-
+util.saveMatrix("Brain_DFT", dft_brain)
 height, width = brain.shape
 brain_size = np.array([height, width])
 
+
+
 p1mask = aqc.bandPattern(cardiac_size, 5, 100, 35)
-util.displayImage(p1mask)
-
+util.saveImage("p1mask", p1mask)
 p1applied = util.applyMask(dft_cardiac, p1mask)
-
 p1image = util.getImage(p1applied)
 util.displayImage(p1image)
+p1fimage = util.post_process_image(p1image)
+util.saveImage("p1_Masked_Image", p1fimage)
+
+
+
+p2mask1 = aqc.bandPattern(cardiac_size, 15, 128, 10)
+p2applied1 = util.applyMask(dft_cardiac, p2mask1)
+p2image1 = util.getImage(p2applied1)
+util.displayImage(p2image1)
+p2fimage1 = util.post_process_image(p2image1)
+util.saveImage("p2_Masked_Image_1", p2fimage1)
+
+p2mask2 = aqc.bandPattern(cardiac_size, 10, 171, 135)
+p2applied2 = util.applyMask(dft_cardiac, p2mask2)
+p2image2 = util.getImage(p2applied2)
+util.displayImage(p2image2)
+p2fimage2 = util.post_process_image(p2image2)
+util.saveImage("p2_Masked_Image_2", p2fimage2)
+
+p2mask3 = aqc.bandPattern(cardiac_size, 30, 100, 270)
+p2applied3 = util.applyMask(dft_cardiac, p2mask3)
+p2image3 = util.getImage(p2applied3)
+util.displayImage(p2image3)
+p2fimage3 = util.post_process_image(p2image3)
+util.saveImage("p2_Masked_Image_3", p2fimage3)
+
+p2mask4 = aqc.bandPattern(cardiac_size, 50, 200, 300)
+p2applied4 = util.applyMask(dft_cardiac, p2mask4)
+p2image4 = util.getImage(p2applied4)
+util.displayImage(p2image4)
+p2fimage4 = util.post_process_image(p2image4)
+util.saveImage("p2_Masked_Image_4", p2fimage4)
 
 
 
 p3mask = aqc.cartesianPattern(cardiac_size, 50)
-util.displayImage(p3mask)
-
 p3applied = util.applyMask(dft_cardiac, p3mask)
-
 p3image = util.getImage(p3applied)
 util.displayImage(p3image)
+p3fimage = util.post_process_image(p3image)
+util.saveImage("p3_Masked_Image", p3fimage)
+
+p3mask10 = aqc.cartesianPattern(cardiac_size, 10)
+p3applied10 = util.applyMask(dft_cardiac, p3mask10)
+p3image10 = util.getImage(p3applied10)
+util.displayImage(p3image10)
+p3fimage10 = util.post_process_image(p3image10)
+util.saveImage("p3_Masked_Image_10%", p3fimage10)
+
+p3mask20 = aqc.cartesianPattern(cardiac_size, 20)
+p3applied20 = util.applyMask(dft_cardiac, p3mask20)
+p3image20 = util.getImage(p3applied20)
+util.displayImage(p3image20)
+p3fimage20 = util.post_process_image(p3image20)
+util.saveImage("p3_Masked_Image_20%", p3fimage20)
+
+p3mask33 = aqc.cartesianPattern(cardiac_size, 33)
+p3applied33 = util.applyMask(dft_cardiac, p3mask33)
+p3image33 = util.getImage(p3applied33)
+util.displayImage(p3image33)
+p3fimage33 = util.post_process_image(p3image33)
+util.saveImage("p3_Masked_Image_33%", p3fimage33)
+
+p3mask75 = aqc.cartesianPattern(cardiac_size, 75)
+p3applied75 = util.applyMask(dft_cardiac, p3mask75)
+p3image75 = util.getImage(p3applied75)
+util.displayImage(p3image75)
+p3fimage75 = util.post_process_image(p3image75)
+util.saveImage("p3_Masked_Image_75%", p3fimage75)
 
 
 
-p4mask = aqc.radialPattern(cardiac_size, 8)
-util.displayImage(p4mask)
 
-p4applied = util.applyMask(dft_cardiac, p4mask)
+p4cm8 = aqc.radialPattern(cardiac_size, 8)
+p4ca8 = util.applyMask(dft_cardiac, p4cm8)
+p4ci8 = util.getImage(p4ca8)
+util.displayImage(p4ci8)
+p4fci8 = util.post_process_image(p4ci8)
+util.saveImage("p4_Cardiac_Masked_Image_8", p4fci8)
 
-p4image = util.getImage(p4applied)
-util.displayImage(p4image)
+p4cm6 = aqc.radialPattern(cardiac_size, 6)
+p4ca6 = util.applyMask(dft_cardiac, p4cm6)
+p4ci6 = util.getImage(p4ca6)
+util.displayImage(p4ci6)
+p4fci6 = util.post_process_image(p4ci6)
+util.saveImage("p4_Cardiac_Masked_Image_6", p4fci6)
+
+p4cm9 = aqc.radialPattern(cardiac_size, 9)
+p4ca9 = util.applyMask(dft_cardiac, p4cm9)
+p4ci9 = util.getImage(p4ca9)
+util.displayImage(p4ci9)
+p4fci9 = util.post_process_image(p4ci9)
+util.saveImage("p4_Cardiac_Masked_Image_9", p4fci9)
+
+p4cm20 = aqc.radialPattern(cardiac_size, 20)
+p4ca20 = util.applyMask(dft_cardiac, p4cm20)
+p4ci20 = util.getImage(p4ca20)
+util.displayImage(p4ci20)
+p4fci20 = util.post_process_image(p4ci20)
+util.saveImage("p4_Cardiac_Masked_Image_20", p4fci20)
+
+
+p4bm8 = aqc.radialPattern(brain_size, 8)
+p4ba8 = util.applyMask(dft_brain, p4bm8)
+p4bi8 = util.getImage(p4ba8)
+util.displayImage(p4bi8)
+p4fbi8 = util.post_process_image(p4bi8)
+util.saveImage("p4_Brain_Masked_Image_8", p4fci8)
+
+p4bm6 = aqc.radialPattern(brain_size, 6)
+p4ba6 = util.applyMask(dft_brain, p4bm6)
+p4bi6 = util.getImage(p4ba6)
+util.displayImage(p4bi6)
+p4fbi6 = util.post_process_image(p4bi6)
+util.saveImage("p4_Brain_Masked_Image_6", p4fbi6)
+
+p4bm9 = aqc.radialPattern(brain_size, 9)
+p4ba9 = util.applyMask(dft_brain, p4bm9)
+p4bi9 = util.getImage(p4ba9)
+util.displayImage(p4bi9)
+p4fbi9 = util.post_process_image(p4bi9)
+util.saveImage("p4_Brain_Masked_Image_9", p4fbi9)
+
+p4bm20 = aqc.radialPattern(brain_size, 20)
+p4ba20 = util.applyMask(dft_brain, p4bm20)
+p4bi20 = util.getImage(p4ba20)
+util.displayImage(p4bi20)
+p4fbi20 = util.post_process_image(p4bi20)
+util.saveImage("p4_Brain_Masked_Image_20", p4fbi20)
+
+
+
+p5mask = aqc.radialPattern(brain_size, 20)
+p5applied = util.applyMask(dft_brain, p5mask)
+p5image = util.getImage(p5applied)
+util.displayImage(p5image)
+p5fimage = util.post_process_image(p5image)
+util.saveImage("p5_Masked_Image", p5fimage)
--- src/interface/ImageNoiseDriver.py
+++ src/interface/ImageNoiseDriver.py
@@ -7,46 +7,47 @@ import src.project.Utilities as util
 
 cardiac = util.loadImage("images/cardiac.jpg")
 util.displayImage(cardiac)
-
 normal_cardiac = util.normalizeImage(cardiac)
-
 dft_cardiac = util.getDFT(normal_cardiac)
-
 height, width = cardiac.shape
 cardiac_size = np.array([height, width])
 
+
 brain = util.loadImage("images/brain.png")
 util.displayImage(brain)
-
 normal_brain = util.normalizeImage(brain)
-
 dft_brain = util.getDFT(normal_brain)
-
 height, width = brain.shape
 brain_size = np.array([height, width])
 
-p6mask = noise.butterworthLowpassFilter(brain_size, 30, 2)
-util.displayImage(p6mask)
 
-p6applied = util.applyMask(dft_brain, p6mask)
-
-p6image = util.getImage(p6applied)
-util.displayImage(p6image)
-
-p7lmask = noise.gaussianLowpassFilter(brain_size, 100)
-util.displayImage(p7lmask)
-
-p7lapplied = util.applyMask(dft_brain, p7lmask)
-
-p7limage = util.getImage(p7lapplied)
-util.displayImage(p7limage)
-
-p7hmask = noise.gaussianHighpassFilter(brain_size, 100)
-util.displayImage(p7hmask)
-
-p7happlied = util.applyMask(dft_brain, p7hmask)
-
-p7himage = util.getImage(p7happlied)
-util.displayImage(p7himage)
+cutoff = np.array([5, 20, 45, 60])
+order = np.array([1, 2, 3, 4])
+for i in cutoff:
+    for j in order:
+        p6mask = noise.butterworthLowpassFilter(brain_size, i, j)
+        p6applied = util.applyMask(dft_brain, p6mask)
+        p6image = util.getImage(p6applied)
+        util.displayImage(p6image)
+        p6fimage = util.post_process_image(p6image)
+        filename = "p6_Masked_Image_" + i + "_" + j
+        util.saveImage(filename, p6fimage)
 
 
+
+for k in range(4):
+    p7lmask = noise.gaussianLowpassFilter(brain_size, 100)
+    p7lapplied = util.applyMask(dft_brain, p7lmask)
+    p7limage = util.getImage(p7lapplied)
+    util.displayImage(p7limage)
+    p7lfimage = util.post_process_image(p7limage)
+    filename = "p7_GLP_Masked_Image_" + k
+    util.saveImage(filename, p7lfimage)
+
+    p7hmask = noise.gaussianHighpassFilter(brain_size, 100)
+    p7happlied = util.applyMask(dft_brain, p7hmask)
+    p7himage = util.getImage(p7happlied)
+    util.displayImage(p7himage)
+    p7hfimage = util.post_process_image(p7himage)
+    filename = "p7_GHP_Masked_Image_" + k
+    util.saveImage(filename, p7hfimage)
